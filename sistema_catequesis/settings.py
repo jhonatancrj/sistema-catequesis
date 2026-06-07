4f4ce6556a4af916e8c6120a113edfd28e4de9a8
chenaoh <jcorderor@fcpn.edu.bo>
2026-06-06 22:37:29 -0400

    primer commit

--- sistema_catequesis/settings.py
+++ sistema_catequesis/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$cx)*r_!d7c7fk^2pw4!vmaq#h0jysub*^(mpkt8rg6sdi@wqv'
-
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-dev-key"
+)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     "web-production-a58f5.up.railway.app",
